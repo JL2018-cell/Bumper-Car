b8ff4b4e5447bec43c96f5c323582c94cd406cb8
Ellen Teng <55531733+ever7-hug@users.noreply.github.com>
2020-11-19 23:45:07 +0800

    Update car.cpp

--- car.cpp
+++ car.cpp
@@ -1,128 +1,216 @@
 #include <cstdlib>
+#include <ctime>
 #include <car.h>
 #include <cmath>
 #include <iostream>
 using namespace std;
-car::car(int x2, int y2)
-{
-	d = 0;
-	v = 0;
-	x = x2 + (rand() % 25);
-	y = y2 + (rand() % 25);
-	elasticity = rand() % 10 + 1;
-	xrate = rand() % 100 + 1;
-	yrate = rand() % 100 + 1;
-	slope = 0;
+
+// input the primary direction from the player's input, no output
+// to update the primary direction of p
+void getdirec(double direc){direction=direc}   
+
+
+// input the primary speed of p from the player's input, no output
+// to update the primary speed of p
+void getv0(double v0_c){v0=v0_c} 
+
+
+// input the difficulty level from the player's input, no output
+// to update the difficulty level
+void getdiff(string diff){difficulty=diff}  
+
+
+// no input, no output
+// set the friction coefficient under different difficulty levels 
+void getu(){
+    if(difficulty=="Easy") u=0;
+    if(difficulty=="Medium" || difficulty=="Hard") u=0.4;
+} 
+
+
+// no input, no output 
+// to generate the random starting positions for cars 
+void car::primary_position(){
+    srand(time(NULL));
+    x = rand()%79+1;
+    y = rand()%39+1; // to avoid the primary positions for p and t to be on the boundaries
 }
 
-int car::getx()
-{
-	return x;
+
+// no input, no output 
+// to avoid the primary positions of p and t are same
+void car::samepos(car & t){
+    while(x==t.x && y==t.y){
+        t.primary_position();
+    }    
 }
 
-int car::gety()
-{
-	return y;
+
+// input the target, no output 
+// to get the primary distance between p and t
+void car::distance(car & t){
+    double dx = t.x - x;
+    double dy = t.x - y;
+    d1 = sqrt( dx * dx + dy * dy);
 }
 
-void car::setx(int x2)
-{
-	x = x2;
+
+// input the target, output the truth value of the direction
+// to check whether the direction is true or not
+bool car::checkdirec(car & t){
+    double truedir; 
+    if(x==t.x){
+        if(t.y>y) truedir=90;
+        if(t.y<y) truedir=-90;
+    }
+
+    if(x!=t.x){
+        double mytan = (t.y-y)/(t.x-x);
+        if(t.x>x) truedir = atan(mytan);
+        if(t.x<x){
+            if(t.y>y) truedir = atan(mytan) + 180;
+            if(t.y==y) truedir = 180;
+            if(t.y<y) truedir = atan(mytan) - 180;
+        }
+    }
+    double err = direction-truedir;
+    if(truedir>178){
+        if(direction>=truedir-2 && direction<=180) return true;
+        else return false;
+    }
+    if(truedir<-178){
+        if(direction>-180 && direction<=truedir+2) return true; //due to boundary limit, at least 2-degree error will be tolerated
+        else return false;
+    }
+    
+    if(direction>=-178 && direction<=178){
+        if(abs(err)<=2) return true; // 4-degree error will be tolerated for non-boundary cases 
+        else return false;
+    }
+   
 }
 
-void car::sety(int y2)
-{
-	y = y2;
+
+//no input, output the speed of p before the moment of collision
+double car::p_speed_beforecol(){
+    if(difficulty=="Easy") v1=v0;
+    if(difficulty=="Medium"||difficulty=="Medium"){
+        if(0.5*v0_c*v0_c>u*d1) v1=sqrt(v0_c*v0_c-2*u*d1);
+        else v1=0;
+    }
+    return v1;
 }
 
-void car::setv(float speed)
-{
-	v = speed;
+
+//no input, output the speed of p after the moment of collsion
+//to get the speed of p after the moment of collsion
+double car::p_speed_aftercol(){
+    if(difficulty=="Easy" || difficulty=="Medium") v2=0;
+    if(difficulty=="Hard") v2=v1/4;
+    return v2;
 }
 
-int car::getelasticity()
-{
-	return elasticity;
+
+//no input, output the speed of t after the moment of collsion
+//to get the speed of t after the moment of collsion
+double car::t_speed_aftercol(){
+    if(difficulty=="Easy" || difficulty=="Medium") v3=v1;
+    if(difficulty=="Hard") v3==3*v1/4;
+    return v3;
+} 
+
+
+//no input, output the distance between t and q(the intercept between the boundary the extension of l(the line connects p and t))
+//to get the distance between t and q
+void car::dist_boundary(car & t){
+    if(x==t.x){
+        if(t.y>y) d2=40-t.y;
+        if(t.y<y) d2=t.y;
+    }
+    if(y==t.y){
+        if(t.x>x) d2=80-t.x;
+        if(t.x<x) d2=t.x;
+    }
+    if(x!=t.x && y!=t.y){
+        double bou_tan;        
+        double k,b; //assume the line connects p and t be y=k*x+b        
+        double xq,yq; // the x,y coordinates of q       
+        k=(t.y-y)/(t.x-x);
+        b= y-k*x;        
+        
+        if(t.x>x && t.y>y){
+            bou_tan = (40-y)/(80-x);           
+            if(bou_tan==k) d2=sqrt((80-t.x)*(80-t.x)+(40-t.y)*(40-t.y));
+            if(k>bou_tan){
+                yq = 40;
+                xq = (yq-b)/k;
+                d2=sqrt((yq-t.y)*(yq-t.y)+(xq-t.x)*(xq-t.x));
+            }
+            if(k<bou_tan){
+                xq=80;
+                yq = k*xq+b;
+                d2=sqrt((yq-t.y)*(yq-t.y)+(xq-t.x)*(xq-t.x));
+            }  
+        }
+        if(t.x>x && t.y<y){
+            bou_tan = (0-y)/(80-x);
+            if(bou_tan==k) d2=sqrt((80-t.x)*(80-t.x)+t.y*t.y);
+            if(k>bou_tan){
+                xq=80;
+                yq = k*xq+b;
+                d2=sqrt((yq-t.y)*(yq-t.y)+(xq-t.x)*(xq-t.x));
+            }
+            if(k<bou_tan){
+                yq=0;
+                xq = (yq-b)/k;
+                d2=sqrt((yq-t.y)*(yq-t.y)+(xq-t.x)*(xq-t.x));
+            }
+            
+        }
+        if(t.x<x && t.y>y){
+            bou_tan = (40-y)/(0-x);
+            if(bou_tan==k) d2=sqrt((40-t.y)*(40-t.y)+t.x*t.x);
+            if(k>bou_tan){
+                xq=0;
+                yq = k*xq+b;
+                d2=sqrt((yq-t.y)*(yq-t.y)+(xq-t.x)*(xq-t.x));
+            }
+            if(k<bou_tan){
+                yq=40;
+                xq = (yq-b)/k;
+                d2=sqrt((yq-t.y)*(yq-t.y)+(xq-t.x)*(xq-t.x));
+            }
+        }
+        if(t.x<x && t.y<y){
+            bou_tan = y/x;
+            if(bou_tan==k) d2=sqrt(t.x*t.x + t.y*t.y);
+            if(k>bou_tan){
+                yq=0;
+                xq = (yq-b)/k;
+                d2=sqrt((yq-t.y)*(yq-t.y)+(xq-t.x)*(xq-t.x));
+            }
+            if(k<bou_tan){
+                xq=0;
+                yq = k*xq+b;
+                d2=sqrt((yq-t.y)*(yq-t.y)+(xq-t.x)*(xq-t.x));
+            }
+        }
+    }
+   
+
 }
 
-void car::setSlope()
-{
-	float i = 1.0;
-	if (slope < 0)
-		i = -1.0;
-	slope = (i * yrate) / xrate;
-}
 
-float car::getv()
-{
-	return v;
-}
+// no input, output the truth value of whether t will be bumped out of the arena or not after collision
+// check whether t will be bumped out of the arena or not after collision
+bool car::check_t_out(){
+    if(0.5*v3*v3>u*d2) return true;
+    else return false;
+}  
 
-double car::getSlope()
-{
-	return slope;
-}
-
-void car::get_direction(int dirc)
-{
-	int d = 100;
-	while (d > 90 || d < -90) {
-		d = dirc - 22 + rand() % 45;
-	}
-}
-
-double car::abs_sub(double a, double b)
-{
-	if (a > b)
-		return a - b;
-	else
-		return b - a;
-}
-
-int car::collided(int carx, int cary)
-{
-	float distance =
-	    sqrt((carx - x) * (carx - x) + (cary - y) * (cary - y));
-	// We set a default collid area as it's a text screen
-	//1: collided, 0:not collided
-	return (distance <= 2);
-}
-
-double car::intcpt()
-{
-	return cpt;
-}
-
-void car::convert(int i)
-{
-	cpt = y - slope * x;
-}
-
-void car::convert()
-{
-	double pii = 3.141592654;
-	slope = tan(((double) (-d) / 180) * pii);
-	cpt = y - slope * x;
-}
-
-float car::duration(float v, float a, float s)
-{
-	float t1, t2, t, v2;
-	v2 = sqrt(v * v + 2 * a * s);
-	//speed after travelling for distance s, decceleration = a, initial speed = v
-	t = (v - v2) / (-a);
-	//time spent during the journey of the bumper car.
-	return t;
-}
-
-// car move on, if it hit edge return 1 else 0
-int car::move()
-{
-	x++;
-	y = slope * x + intcpt();
-	if (x > 80 || y > 40 || y < 0) {
-		return 1;
-	}
-	return 0;
-
+// no input, output the truth value of whether p will remain in the arena or not after collision
+// check whether p will remain in the arena or not after collision
+bool car::check_p_remain(){
+    if(0.5*v2*v2<=u*d2) return true;
+    else return false 
 }
